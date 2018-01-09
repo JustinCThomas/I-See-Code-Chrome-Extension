f0f3170bedfc6d2840cd0e7db50285d650c9a253
Justin Thomas <ThomasCJustin@gmail.com>
2018-01-09 13:32:22 -0500

    Change color of anchor tags to be more noticeable

--- popup.js
+++ popup.js
@@ -3,10 +3,14 @@ var toggleButton = document.getElementById("toggleButton");
 
 toggleButton.addEventListener('click', function() {
   chrome.tabs.executeScript({
-    code: `var elements = document.querySelectorAll("*")
+    code: `var elements = document.querySelectorAll("*");
+    var anchorTags = document.querySelectorAll("a");
     elements.forEach((elem) => {
       elem.classList.toggle('code');
-    })
+    });
+    anchorTags.forEach((a) => {
+      a.classList.toggle('anchor');
+    });
     `
   });
 });
