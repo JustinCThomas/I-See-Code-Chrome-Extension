c94d513957b3f1de5d4fee14b821da609ab049ee
Justin Thomas <ThomasCJustin@gmail.com>
2018-01-09 14:40:30 -0500

    Simplify Code Logic

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
-var toggleButton = document.getElementById("toggleButton");
+let toggleButton = document.getElementById("toggleButton");
 
 
 toggleButton.addEventListener('click', function() {
   chrome.tabs.executeScript({
     code: `var elements = document.querySelectorAll("*");
-    var anchorTags = document.querySelectorAll("a");
     elements.forEach((elem) => {
-      elem.classList.toggle('code');
-    });
-    anchorTags.forEach((a) => {
-      a.classList.toggle('anchor');
+      if (elem.nodeName.toLowerCase() !== "a") {
+        elem.classList.toggle('code');
+      } else {
+        elem.classList.toggle('code-anchor');
+      }
     });
     `
   });
