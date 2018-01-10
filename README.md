6a7d9a10110b94aa6055b463e53edacbe537bf27
Justin Thomas <ThomasCJustin@gmail.com>
2018-01-10 15:22:09 -0500

    Remove Extension Popup, Remove popup.html

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This is a basic chrome extension.
 
-Click the button to toggle the code vision on and off.
+Click the extension icon to toggle the code vision on and off.
 
 Does not work properly on all sites.
--- popup.js
+++ popup.js
@@ -1,21 +1,4 @@
-// let toggleButton = document.getElementById("toggleButton");
-//
-//
-// toggleButton.addEventListener('click', function() {
-//   chrome.tabs.executeScript({
-//     code: `var elements = document.querySelectorAll("*");
-//     elements.forEach((elem) => {
-//       if (elem.nodeName.toLowerCase() !== "a") {
-//         elem.classList.toggle('code');
-//       } else {
-//         elem.classList.toggle('code-anchor');
-//       }
-//     });
-//     `
-//   });
-// });
-
-window.addEventListener('load', () => {
+chrome.browserAction.onClicked.addListener( (tab) => {
   chrome.tabs.executeScript({
     code: `var elements = document.querySelectorAll("*");
     elements.forEach((elem) => {
